c2448d80deca6e34faf73d0493cdaa29d0e8f046
papa-react <makiya112545@gmail.com>
2022-01-26 14:59:28 -1200

    set up image domain image size and device size

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["links.papareact.com"],
+		deviceSizes: [640, 750, 828, 1080, 1200],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		formats: ["image/avif", "image/webp"],
+	},
+};
 
 module.exports = nextConfig
