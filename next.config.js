d3825f51eb1f20f11658ba93131cd7b5332eb020
papa-react <makiya112545@gmail.com>
2022-01-28 02:29:41 -1200

    add sanity cdn

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["links.papareact.com"],
+		domains: ['links.papareact.com','cdn.sanity.io'],
 		deviceSizes: [640, 750, 828, 1080, 1200],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 		formats: ["image/avif", "image/webp"],
