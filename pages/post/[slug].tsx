0febe6b72c13870a853dff9327df14e85089edc1
papa-react <makiya112545@gmail.com>
2022-01-29 04:45:14 -1200

    style Image

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,25 +1,110 @@
 import { GetStaticProps } from "next";
 import { useNextSanityImage } from "next-sanity-image";
-import { useRouter } from "next/router";
+import Img from "next/image";
 import Header from "../../components/Header";
 import { configuredSanityClient } from "../../components/post";
 import { sanityClient } from "../../lib/sanity";
+import PortableText from "react-portable-text";
 import { Post } from "../../type";
+import { blockContentToPlainText } from "react-portable-text";
 
-interface Props{ 
-	post:Post
+interface Props {
+	post: Post;
 }
-
 const Post = ({ post }: Props) => {
-
-	// const mainImage = useNextSanityImage(
-	// 	configuredSanityClient,
-	// 	post.mainImage
-	// );
-
+	const mainImage = useNextSanityImage(
+		configuredSanityClient,
+		post.mainImage
+	);
+	const profile = useNextSanityImage(
+		configuredSanityClient,
+		post.author.image
+	);
+		console.log(post.body);
+		
 	return (
 		<div>
 			<Header />
+			<main className="max-w-[730px] mx-auto  sm:p-5  space-y-5 md:space-y-8">
+				<div>
+					<h1 className=" text-4xl sm:text-5xl mt-10 mb-3 font-extrabold text-gray-800 capitalize">
+						{post.title}
+					</h1>
+					{/* author profile */}
+					<div className="flex items-center mt-5 space-x-3">
+						{post.author.image && (
+							<div className="w-8 h-8 relative rounded-full overflow-hidden shrink-0 ">
+								<Img
+									src={profile.src}
+									loader={profile.loader}
+									layout="fill"
+									objectFit="cover"
+								/>
+							</div>
+						)}
+						<span className="text-sm font-mono text-gray-400">
+							{post.author.name}
+						</span>
+						<span className="text-sm font-mono text-gray-400">
+							{new Date(post._createdAt).toDateString()}
+						</span>
+					</div>
+				</div>
+				{post.mainImage && (
+					<div className="w-full h-[330px] xl:h-[500px] bg-gray-200 relative  overflow-hidden  ">
+						<Img
+							className="group-hover:scale-105 transition-transform duration-200 transform-gpu ease-in-out"
+							src={mainImage.src}
+							loader={mainImage.loader}
+							layout="fill"
+							quality={100}
+							objectFit="cover"
+							alt={post.title}
+						/>
+					</div>
+				)}
+				{/* description */}
+				<h3 className="text-sm md:text-xl font-normal text-gray-600">
+					{post.description}
+				</h3>
+				<div>
+					<PortableText
+						content={post.body}
+						projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+						dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+						serializers={{
+							h1: (props: any) => (
+								<h1
+									className="text-2xl font-bold my-5 text-gray-500"
+									{...props}
+								/>
+							),
+							h2: (props: any) => (
+								<h1
+									className="text-xl font-bold my-5 text-gray-500"
+									{...props}
+								/>
+							),
+							p: (props: any) => (
+								<p
+									className="text-xl font-bold my-5 text-gray-500"
+									{...props}
+								/>
+							),
+							li: ({ children }: any) => (
+								<li className="ml-4 list-disc" {...children} />
+							),
+							link: ({ href, children }: any) => (
+								<a
+									href={href}
+									className="text-blue-500 hover:underline"
+									{...children}
+								/>
+							),
+						}}
+					/>
+				</div>
+			</main>
 		</div>
 	);
 };
@@ -46,7 +131,6 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-	
 	const query = `*[_type == 'post' && slug.current == $slug  ][0]{ 
 		_id, 
 		title,
@@ -59,7 +143,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 			slug,
 			image,
 			bio
-		}
+		},
+		body,
 	} `;
 
 	const post = await sanityClient.fetch(query, { slug: params?.slug });
@@ -73,7 +158,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 		// Next.js will attempt to re-generate the page:
 		// - When a request comes in
 		// - At most once every 10 seconds
-		revalidate: 10, // In seconds
+		revalidate: 300, // In seconds
 	};
 };
 
